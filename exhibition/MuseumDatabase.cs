8ec176445a54f4eed23a435ecaff3b033871f705
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-05-11 08:59:17 +0500

    Update MuseumDatabase.cs

--- exhibition/MuseumDatabase.cs
+++ exhibition/MuseumDatabase.cs
@@ -1,82 +1,65 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
 
-public static class MuseumDatabase
-{
+public static class MuseumDatabase {
     private static string fileName = "museum.dat";
-
     public static List<Exhibit> exhibits = new List<Exhibit>();
-
-    // Загрузка
-    public static void Load()
-    {
+    
+    
+    public static void Load() {
         if (!File.Exists(fileName))
             return;
 
-        try
-        {
+        try {
             FileStream fs = new FileStream(fileName, FileMode.Open);
-
-#pragma warning disable SYSLIB0011
+            
             BinaryFormatter bf = new BinaryFormatter();
             exhibits = (List<Exhibit>)bf.Deserialize(fs);
-#pragma warning restore SYSLIB0011
 
             fs.Close();
         }
-        catch
-        {
+        catch {
             Console.WriteLine("Ошибка чтения файла.");
         }
     }
 
-    // Сохранение
-    public static void Save()
-    {
-        try
-        {
-            FileStream fs = new FileStream(fileName, FileMode.Create);
 
-#pragma warning disable SYSLIB0011
+    public static void Save() {
+        try {
+            FileStream fs = new FileStream(fileName, FileMode.Create);
+            
             BinaryFormatter bf = new BinaryFormatter();
             bf.Serialize(fs, exhibits);
-#pragma warning restore SYSLIB0011
 
             fs.Close();
         }
-        catch
-        {
+        catch {
             Console.WriteLine("Ошибка сохранения.");
         }
     }
 
-    // Добавление
-    public static void Add(Exhibit exhibit)
-    {
+
+    public static void Add(Exhibit exhibit) {
         exhibits.Add(exhibit);
     }
 
-    // Просмотр
-    public static void ShowAll()
-    {
-        if (!exhibits.Any())
-        {
+
+    public static void ShowAll() {
+        if (!exhibits.Any()) {
             Console.WriteLine("База данных пуста.");
             return;
         }
 
-        foreach (var exhibit in exhibits)
-        {
+        foreach (var exhibit in exhibits) {
             Console.WriteLine(exhibit);
         }
     }
 
-    // Удаление по ключу
-    public static bool DeleteById(int id)
-    {
+
+    public static bool DeleteById(int id) {
         var exhibit = exhibits.FirstOrDefault(x => x.Id == id);
 
         if (exhibit == null)
@@ -86,33 +69,28 @@ public static class MuseumDatabase
         return true;
     }
 
-    // ===== ЗАПРОСЫ =====
+    
 
-    // 1. Перечень экспонатов определенного века
-    public static IEnumerable<Exhibit> GetByCentury(int century)
-    {
+    public static IEnumerable<Exhibit> GetByCentury(int century) {
         return exhibits.Where(x => x.Century == century);
     }
 
-    // 2. Перечень экспонатов по первой букве
-    public static IEnumerable<Exhibit> GetByFirstLetter(char letter)
-    {
+
+    public static IEnumerable<Exhibit> GetByFirstLetter(char letter) {
         return exhibits.Where(x =>
             x.Name.StartsWith(letter.ToString(),
             StringComparison.OrdinalIgnoreCase));
     }
 
-    // 3. Проверка наличия экспоната
-    public static bool Exists(string name)
-    {
+
+    public static bool Exists(string name) {
         return exhibits.Any(x =>
             x.Name.Equals(name,
             StringComparison.OrdinalIgnoreCase));
     }
 
-    // 4. Количество экспонатов
-    public static int CountExhibits()
-    {
+
+    public static int CountExhibits() {
         return exhibits.Count();
     }
 }
