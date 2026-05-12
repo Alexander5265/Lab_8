2e131c64b5f8e287286017d3a97f2344b2a208b2
Alexander5265 <104996703+Alexander5265@users.noreply.github.com>
2026-05-12 17:57:24 +0500

    Update Museum.cs

--- exhibition/Museum.cs
+++ exhibition/Museum.cs
@@ -1,18 +1,31 @@
 using System;
 
-public class Exhibit {
+[Serializable]
+internal class Exhibit
+{
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Author { get; set; }
-    public int Century { get; set; }
-    public DateTime CreationDate { get; set; }
-    public double EstimatedValue { get; set; }
-    public bool IsOnDisplay { get; set; }
-    
 
-    public Exhibit(int id, string name, string author,
-        int century, DateTime creationDate,
-        double estimatedValue, bool isOnDisplay) {
+    public string Name { get; set; }
+
+    public string Author { get; set; }
+
+    public int Century { get; set; }
+
+    public DateTime CreationDate { get; set; }
+
+    public double EstimatedValue { get; set; }
+
+    public bool IsOnDisplay { get; set; }
+
+    public Exhibit(
+        int id,
+        string name,
+        string author,
+        int century,
+        DateTime creationDate,
+        double estimatedValue,
+        bool isOnDisplay)
+    {
         Id = id;
         Name = name;
         Author = author;
@@ -22,13 +35,15 @@ public class Exhibit {
         IsOnDisplay = isOnDisplay;
     }
 
-    public override string ToString() {
-        return $"ID: {Id}\n" +
-               $"Название: {Name}\n" +
-               $"Автор: {Author}\n" +
-               $"Век: {Century}\n" +
-               $"Дата создания: {CreationDate.ToShortDateString()}\n" +
-               $"Стоимость: {EstimatedValue}\n" +
-               $"На выставке: {(IsOnDisplay ? "Да" : "Нет")}\n";
+    public override string ToString()
+    {
+        return
+            $"ID: {Id}\n" +
+            $"Название: {Name}\n" +
+            $"Автор: {Author}\n" +
+            $"Век: {Century}\n" +
+            $"Дата создания: {CreationDate.ToShortDateString()}\n" +
+            $"Стоимость: {EstimatedValue}\n" +
+            $"На выставке: {(IsOnDisplay ? "Да" : "Нет")}\n";
     }
 }
